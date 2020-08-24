ee00b576e11ca03742696a9d5ede2658093d27ec
epsakzm <parkhw0614@gmail.com>
2020-08-24 19:41:03 +0900

    dfs

---
+++ week7/network.cpp
@@ -0,0 +1,40 @@
+// https://programmers.co.kr/learn/courses/30/lessons/43162
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int answer = 0;
+bool visited[201];
+
+void dfs(int x, int n, vector<vector<int>> computers)
+{
+    visited[x] = true;
+    for (int i = 0; i < n; i++)
+    {
+        if (computers[x][i] && !visited[i])
+        {
+            dfs(i, n, computers);
+        }
+    }
+}
+
+int solution(int n, vector<vector<int>> computers)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            answer++;
+            dfs(i, n, computers);
+        }
+    }
+    return answer;
+}
+
+int main(void)
+{
+    vector<vector<int>> v = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+    cout << solution(1, v) << endl;
+    return 0;
+}
