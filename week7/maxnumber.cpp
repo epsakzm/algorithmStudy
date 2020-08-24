e5cda0d723832974616fb09a28d666094b4973c1
epsakzm <parkhw0614@gmail.com>
2020-08-24 19:41:48 +0900

    greedy

---
+++ week7/maxnumber.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> v;
+
+string solution(string number, int k)
+{
+    string answer = "";
+    int N = number.length() - k; // 6
+
+    while (N--)
+    {
+        int maxNumber, last;
+        maxNumber = last = 0;
+        for (int j = 0; j < number.length() - N; j++)
+        //0, 1, 2, 3, 4
+        {
+            if (maxNumber < number[j])
+            {
+                maxNumber = number[j];
+                last = j;
+            }
+        }
+        answer += maxNumber;
+        number = number.substr(last+1, -1);
+    }
+    return answer;
+}
+
+int main(void)
+{
+    cout << solution("4177252841", 4) << endl;
+    return 0;
+}
