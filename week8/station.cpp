d0c92a90e3c4c5cfdf82da2901cfa9c67d3cbcd6
epsakzm <parkhw0614@gmail.com>
2020-08-31 19:09:12 +0900

    greedy

---
+++ week8/station.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solution(int n, vector<int> stations, int w)
+{
+    int answer = 0;
+    int pos = 1;
+    int sIdx = 0;
+    while(pos <= n){
+        if(pos >= stations[sIdx] - w && pos <=stations[sIdx] + w)
+            pos = stations[sIdx++] + w + 1;
+        else{
+            pos += w * 2 + 1;
+            answer++;
+        }
+    }
+    return answer;
+}
+
+int main(void)
+{
+    cout << solution(16, {9}, 2);
+    return 0;
+}
